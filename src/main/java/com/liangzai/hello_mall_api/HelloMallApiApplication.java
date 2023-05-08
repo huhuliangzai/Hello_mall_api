d0f72042d0b1f97d28f5530b8d11385961b0755c
liangzai <1821995845@qq.com>
2023-05-08 22:43:03 +0800

    :tada:

---
+++ src/main/java/com/liangzai/hello_mall_api/HelloMallApiApplication.java
@@ -0,0 +1,13 @@
+package com.liangzai.hello_mall_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMallApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMallApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liangzai/hello_mall_api/HelloMallApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liangzai.hello_mall_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMallApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
